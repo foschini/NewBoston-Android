c4a21493a08dfe26c0e7306a8b20e357b4556d5d
Andrea Foschini <foschini@gmail.com>
2014-08-25 16:05:24 +0200

    Lesson 8 Completed

--- src/com/thenewboston/travis/StartingPoint.java
+++ src/com/thenewboston/travis/StartingPoint.java
@@ -3,13 +3,28 @@ package com.thenewboston.travis;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class StartingPoint extends Activity {
 
+	int counter;
+	Button add,sub;
+	TextView display;
+	
+	
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_starting_point);
+		counter=0;
+		
+		add = (Button) findViewById(R.id.bAdd);
+		sub = (Button) findViewById(R.id.bSub);
+		display = (TextView) findViewById(R.id.tvDisplay);
+		
 	}
 
 	@Override
